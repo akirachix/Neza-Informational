ad3217a6c23999a8e7985cc2ca83ccbf5f90174c
NjeriMarion <njeriwmarion@gmail.com>
2023-09-06 22:31:44 +0300

    Combined products and homepage

--- neza-informationa-lwebsite/src/App.js
+++ neza-informationa-lwebsite/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './components/home';
+import Product from './components/Product';
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Product/>
     </div>
   );
 }
---
+++ neza-informationa-lwebsite/src/components/Product/index.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import Product from '.';
+
+describe("Product component", () => {
+  it("renders the Product component without errors and displays it as expected" ,()=>{
+    render(<Product />);
+  
+   
+});
+
+});
+    
