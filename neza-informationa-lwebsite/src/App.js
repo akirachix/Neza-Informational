c3bf014142910689c55977622cf4499702fd033e
Wambui-Mwangi <mwangiestherwambuii@gmail.com>
2023-09-06 22:32:03 +0300

    Team updates

--- neza-informationa-lwebsite/src/App.js
+++ neza-informationa-lwebsite/src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
-<<<<<<< HEAD
 import TheTeam from './components/team';
 
 function App() {
   return (
     <div className='App'>
       <TheTeam/>
-=======
-import TheTeam from './components/team';
-
-function App() {
-  return (
-    <div className='App'>
-      <TheTeam/>
->>>>>>> 66cbc175601c36cbca4537abd980b7b68e4e112a
     </div>
   );
 }
