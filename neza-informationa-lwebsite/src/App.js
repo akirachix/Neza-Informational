0a453bd2cc48be454fbecf2ac7866318bc2cfa75
Student <mwauloice@gmail.com>
2023-09-07 16:06:27 +0300

    made changes to the media queries

--- neza-informationa-lwebsite/src/App.js
+++ neza-informationa-lwebsite/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
-import Layout from './components/layout';
+import Layout from './components/Layout';
+import TheTeam from './components/team';
+import Product from './components/Product';
+import Home from './components/home';
 
 function App() {
   return (
     <div className="App">
       <Layout/>
+      <Home/>
+      <Product/>
+      <TheTeam/>
     </div>
   );
 }
