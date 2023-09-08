ec425aae58effd72f935b46483f216c393a02182
Student <mwauloice@gmail.com>
2023-09-08 17:42:49 +0300

    made changes to the navbar

--- neza-informationa-lwebsite/src/components/Navigation/index.js
+++ neza-informationa-lwebsite/src/components/Navigation/index.js
@@ -1,35 +1,54 @@
-
-import React, { useState } from "react";
-
-import "./style.css";
-const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+import React, { useState } from 'react';
+import './style.css';
+function Navbar() {
+  const [isNavOpen, setNavOpen] = useState(false);
+  const toggleNav = () => {
+    setNavOpen(!isNavOpen);
+  };
   return (
-    <div className="Navbar">
-      <span className="logo">
-      <img src="/images/LOGO.png" alt="NEZA LOGO" />
-      </span>
-      <div className={`nav-items ${isOpen && "open"}`}>
-        <a href="../home">Home</a>
-        <a href="../Product">About</a>
-        <a href="../team"> Team</a>
-        <a href="/contact">Contact</a>
-        <button className="button">Register</button>
+    <nav>
+      <div className={`navbar-wrapper ${isNavOpen ? 'nav-open' : ''}`}>
+        <span className="brand-container">
+          <a className="logo-link" href="#logo">
+            <img
+              src={ '/images/LOGO.png'}
+              alt="Logo"
+              className="logo-img"
+            />
+          </a>
+        </span>
+        <div
+          data-testid="hamburger-menu"
+          className={`hamburger-menu ${isNavOpen ? 'open' : ''}`}
+          onClick={toggleNav}
+        >
+          {isNavOpen ? (
+            <div className="close-icon">
+              <span></span>
+              <span></span>
+            </div>
+          ) : (
+            <>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+            </>
+          )}
+        </div>
+        <ul data-testid="nav-list" className={`nav-list ${isNavOpen ? 'open' : ''}`}>
+          <div className="centered-links">
+            <li><a href="#Home">Home</a></li>
+            <li><a href="#About">About</a></li>
+            <li><a href="#Team">Team</a></li>
+            <li><a href="#Contact">Contact</a> </li>
+            <div><button>Register</button></div>
+           
+           
+           
+          </div>
+        </ul>
       </div>
-      <div
-        className={`nav-toggle ${isOpen && "open"}`}
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <div className="bar"></div>
-      </div>
-    </div>
+    </nav>
   );
-};
+}
 export default Navbar;
-
-
-
-
-
-
-
