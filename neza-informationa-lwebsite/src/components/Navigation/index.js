4bcea1c5192cc0e5879efa70e77c0c64390a0ea0
Student <mwauloice@gmail.com>
2023-09-09 08:37:39 +0300

    made changes to the navbar

--- neza-informationa-lwebsite/src/components/Navigation/index.js
+++ neza-informationa-lwebsite/src/components/Navigation/index.js
@@ -1,29 +1,55 @@
 import React, { useState } from "react";
 import "./styles.css";
-
-
-
-const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+function Navbar() {
+  const [active, setActive] = useState("nav__menu");
+  const [icon, setIcon] = useState("nav__toggler");
+  const navToggle = () => {
+    if (active === "nav__menu") {
+      setActive("nav__menu nav__active");
+    } else setActive("nav__menu");
+    // Icon Toggler
+    if (icon === "nav__toggler") {
+      setIcon("nav__toggler toggle");
+    } else setIcon("nav__toggler");
+  };
   return (
-    <div className="Navbar">
-      <span className="logo">
-      <img src="/images/LOGO.png" alt="NEZA LOGO"  />
-      </span>
-      <div className={`nav-items ${isOpen && "open"}`}>
-        <a href="/">Home</a>
-        <a href="#product">About</a>
-        <a href="/Theteam"> Team</a>
-        <a href="/Footer">Contact</a>
-        <button className="button">Register</button>
+    <nav className="nav">
+      <a href="/" className="nav__brand">
+        <img src="/images/LOGO.png" alt="img"/>
+      </a>
+      <ul className={active}>
+        <li className="nav__item">
+          <a href="/home" className="nav__link">
+            Home
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/about" className="nav__link">
+            About us
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="/portfolio" className="nav__link">
+            Team
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="skills" className="nav__link">
+            Contact Us
+          </a>
+        </li>
+        <li className="nav__item">
+          <a href="contact" className="nav__link">
+            <button>Register</button>
+          </a>
+        </li>
+      </ul>
+      <div onClick={navToggle} className={icon}>
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
       </div>
-      <div
-        className={`nav-toggle ${isOpen && "open"}`}
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <div className="bar"></div>
-      </div>
-    </div>
+    </nav>
   );
-};
+}
 export default Navbar;
